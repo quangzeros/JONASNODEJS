063be20259bfeafd16a460c42b6ad460f3bf7dc4
quangzeros <nguyenkhacquang176@gmail.com>
2023-01-01 21:13:54 +0700

    last updates

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -69,9 +69,9 @@ exports.isUserBookingTour = catchAsync(async(req,res,next)=>{
     next();
 })
 const createBookingCheckout = async session  => {
-    const tour = session.client_reference_id;
-    const user = (await User.findOne({email:session.customer_email})).id;
-    const price = session.line_items[0].price_data.unit_amount / 100 ;
+    const tour = session.customer_reference_id
+    const user = (await User.findOne({email:session.customer_details.email})).id;
+    const price = session.amount_total / 100 ;
     await Booking.create({tour,user,price});
 }
 
