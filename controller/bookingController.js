e98f6073fc0d05628eb2a9eb5217cbf08f8a7518
quangzeros <nguyenkhacquang176@gmail.com>
2023-01-01 17:44:10 +0700

    update bokingss

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -46,8 +46,7 @@ exports.createBookingCheckout = catchAsync( async (req,res,next) =>{
     if (!tour && !user && !price ){
         return next()
     }
-    const startAtTime = new Date(startAt)
-    await Booking.create({tour,user,price,startAtTime})
+    await Booking.create({tour,user,price})
 
     res.redirect(req.originalUrl.split('?')[0])
 })
